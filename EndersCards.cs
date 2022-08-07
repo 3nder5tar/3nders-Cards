f4e509ce85e59e15123f6051cb3139e08bf05fd1
3nder5tar <enderstardragon@gmail.com>
2022-08-07 13:43:10 -0700

    My mod cards

---
+++ EndersCards.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using EndersCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace EndersCards
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class EndersCards : BaseUnityPlugin
+    {
+        private const string ModId = "com.3nder5tar.EndersCards";
+        private const string ModName = "EndersCards";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "EC";
+
+        public static EndersCards instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<MyCardName>();
+        }
+    }
+}
